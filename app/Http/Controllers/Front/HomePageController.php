678da0c45884830038f9538916ccc259946dd3da
Hadi <alrehman24@gmail.com>
2025-05-30 01:44:30 +0500

    working on Home dataapi

---
+++ app/Http/Controllers/Front/HomePageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\HomeBanner;
+use Illuminate\Http\Request;
+use App\Traits\ApiResponse;
+
+class HomePageController extends Controller
+{
+    use ApiResponse;
+    public function getHomeData()
+    {
+        $data['banner'] =HomeBanner::get();
+        $data['categories'] =Category::with('products')->get();
+        $data['brands']=Brand::get();
+        return $this->success(['data'=>$data],'Profile Updated Successfully');
+    }
+}
